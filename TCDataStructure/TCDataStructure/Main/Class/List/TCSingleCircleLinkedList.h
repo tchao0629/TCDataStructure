c1f1ae11ed46a3ce93c54d8c7f76e0ff78b9c19c
xiaochaoKF <tangchao_20170629@163.com>
2021-07-06 09:11:39 +0800

    新增单向循环链表

---
+++ TCDataStructure/TCDataStructure/Main/Class/List/TCSingleCircleLinkedList.h
@@ -0,0 +1,17 @@
+//
+//  TCSingleCircleLinkedList.h
+//  MyTest
+//
+//  Created by tangchao on 2021/7/6.
+//  Copyright © 2021 tangchao. All rights reserved.
+//
+
+#import "TCAbstractList.h"
+
+NS_ASSUME_NONNULL_BEGIN
+/** 单向循环链表 */
+@interface TCSingleCircleLinkedList<TCElement> : TCAbstractList<TCElement>
+
+@end
+
+NS_ASSUME_NONNULL_END
