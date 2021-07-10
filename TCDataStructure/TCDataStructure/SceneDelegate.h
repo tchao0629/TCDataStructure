ed640d1b8d65322c9267f9993dd90de6da020093
tangchao <tchao@ykdeMacBook-Pro.local>
2021-07-10 08:51:48 +0800

    修改AppDelegate

--- TCDataStructure/TCDataStructure/SceneDelegate.h
+++ TCDataStructure/TCDataStructure/SceneDelegate.h
@@ -8,9 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
-
-@property (strong, nonatomic) UIWindow * window;
-
-@end
+//@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+//
+//@property (strong, nonatomic) UIWindow * window;
+//
+//@end
 
